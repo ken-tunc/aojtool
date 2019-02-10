ac8510c30e05b2f002ab46f0ac076006ad0e4eea
ken_tunc <tunc.adfuisses.53@gmail.com>
2019-02-11 05:22:52 +0900

    Add login subcommand.

---
+++ commands/login.go
@@ -0,0 +1,58 @@
+package commands
+
+import (
+	"context"
+	"fmt"
+	"syscall"
+
+	"golang.org/x/crypto/ssh/terminal"
+
+	"github.com/ken-tunc/aojtool/api"
+	"github.com/spf13/cobra"
+)
+
+var loginCmd = &cobra.Command{
+	Use:   "login",
+	Short: "Login to Aizu Online Judge.",
+	Run: func(cmd *cobra.Command, args []string) {
+		client, err := api.NewClient()
+		if err != nil {
+			abort(err)
+		}
+
+		ctx := context.Background()
+		loggedIn, err := client.Auth.IsLoggedIn(ctx)
+		if err != nil {
+			abort(err)
+		}
+
+		if loggedIn {
+			return
+		}
+
+		var id string
+		cmd.Print("AOJ user id: ")
+		_, err = fmt.Scan(&id)
+		if err != nil {
+			abort(err)
+		}
+
+		cmd.Print("password: ")
+		bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+		if err != nil {
+			abort(err)
+		}
+
+		password := string(bytePassword)
+
+		ctx = context.Background()
+		_, err = client.Auth.Login(ctx, id, password)
+		if err != nil {
+			abort(err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(loginCmd)
+}
